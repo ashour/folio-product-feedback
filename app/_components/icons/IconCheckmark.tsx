916a5aabeec625404d989e8ed77d45a4ef429dd1
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-06-03 19:27:11 +0100

    chore: add icons from figma

---
+++ app/_components/icons/IconCheckmark.tsx
@@ -0,0 +1,13 @@
+export default function IconCheckmark() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M3 8.23287L6.52154 12L14 4" stroke="#AD1FEA" strokeWidth="2" />
+    </svg>
+  );
+}
---
+++ app/_components/icons/IconPlusSign.tsx
@@ -0,0 +1,16 @@
+export default function IconPlusSign() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.3425 24V18.1657H24.0295V13.8637H18.3425V8H13.7459V13.8637H8V18.1657H13.7459V24H18.3425Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
