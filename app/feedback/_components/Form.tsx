0eaffe6a423bc5b71b9527802f732fdba00603cd
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-06-05 11:07:56 +0100

    chore: use framer motion for animation

--- app/feedback/_components/Form.tsx
+++ app/feedback/_components/Form.tsx
@@ -9,9 +9,9 @@ import {
   ListboxButton,
   ListboxOption,
   ListboxOptions,
-  Transition,
 } from "@headlessui/react";
 
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import HelpText from "./HelpText";
 import Label from "./Label";
@@ -42,31 +42,38 @@ export default function Form() {
           onChange={setSelectedCategory}
           name="category"
         >
-          <ListboxButton className="form-input relative mb-6 flex w-full items-baseline justify-between text-left focus:outline-none focus:ring-[1px] focus:ring-blue">
-            {selectedCategory}
-            <IconChevronUp className="-scale-y-100" />
-          </ListboxButton>
-          <Transition
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <ListboxOptions
-              anchor="bottom"
-              className="shadow-listbox w-[var(--button-width)] rounded-10px [--anchor-gap:16px]"
-            >
-              {categories.map((category) => (
-                <ListboxOption
-                  key={category}
-                  value={category}
-                  className="group flex items-baseline justify-between border-b border-slate-600/15 bg-white px-6 py-3 text-body-1 text-slate-500 last:border-b-0 data-[focus]:cursor-pointer data-[focus]:text-purple-400"
-                >
-                  {category}
-                  <IconCheckmark className="invisible group-data-[selected]:visible" />
-                </ListboxOption>
-              ))}
-            </ListboxOptions>
-          </Transition>
+          {({ open }) => (
+            <>
+              <ListboxButton className="form-input relative mb-6 flex w-full items-baseline justify-between text-left focus:outline-none focus:ring-[1px] focus:ring-blue">
+                {selectedCategory}
+                <IconChevronUp className="-scale-y-100" />
+              </ListboxButton>
+              <AnimatePresence>
+                {open && (
+                  <ListboxOptions
+                    static
+                    as={motion.div}
+                    initial={{ opacity: 0, scale: 0.95 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0.95 }}
+                    anchor="bottom"
+                    className="shadow-listbox w-[var(--button-width)] origin-top rounded-10px [--anchor-gap:16px]"
+                  >
+                    {categories.map((category) => (
+                      <ListboxOption
+                        key={category}
+                        value={category}
+                        className="group flex items-baseline justify-between border-b border-slate-600/15 bg-white px-6 py-3 text-body-1 text-slate-500 last:border-b-0 data-[focus]:cursor-pointer data-[focus]:text-purple-400"
+                      >
+                        {category}
+                        <IconCheckmark className="invisible group-data-[selected]:visible" />
+                      </ListboxOption>
+                    ))}
+                  </ListboxOptions>
+                )}
+              </AnimatePresence>
+            </>
+          )}
         </Listbox>
       </Field>
     </form>
