d52e162d61c8d23705af5fad736c5014dbf742b5
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-06-12 19:05:06 +0100

    feat: add loading indicator to feedback index

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,38 +1,23 @@
-import { type Feedback } from "@prisma/client";
-import ErrorAlert from "../_components/ErrorAlert";
-import FeedbackItem from "../_components/FeedbackItem";
+import { Suspense } from "react";
 import NavLayout from "../_layout/NavLayout";
-import EmptyState from "./_components/EmptyState";
+import FeedbackIndex from "./_components/FeedbackIndex";
 import TopControlBar from "./_components/TopControlBar";
-import { fetchFeedback } from "./_lib/fetchFeedback";
 
 export default async function Home() {
-  let feedbackItems: Feedback[] | undefined;
-  let error: string | undefined;
-
-  try {
-    feedbackItems = await fetchFeedback();
-  } catch (err) {
-    error = (err as Error).message;
-  }
-
-  const hasFeedbackItems = feedbackItems && feedbackItems.length > 0;
-
   return (
     <NavLayout>
       <TopControlBar />
 
       <main className="mx-6 mt-8 md:mx-0 lg:mt-6">
-        {error && <ErrorAlert>{error}</ErrorAlert>}
-        {!hasFeedbackItems && <EmptyState />}
-
-        {hasFeedbackItems && (
-          <section className="flex flex-col gap-3 pb-14">
-            {feedbackItems!.map((feedback) => (
-              <FeedbackItem key={feedback.id} feedback={feedback} />
-            ))}
-          </section>
-        )}
+        <Suspense
+          fallback={
+            <div className="rounded-10px bg-white py-3 text-center text-slate-600">
+              Loading feedback...
+            </div>
+          }
+        >
+          <FeedbackIndex />
+        </Suspense>
       </main>
     </NavLayout>
   );
