4151b0ed87d6526086a783ef2a223615c7f37347
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-06-14 20:47:12 +0100

    refactor: simplify body scroll logic for modal

--- app/_components/FormModal.tsx
+++ app/_components/FormModal.tsx
@@ -17,12 +17,8 @@ export default function FormModal({
   useEffect(() => {
     if (isModalOpen) {
       document.body.style.position = "fixed";
-      document.body.style.top = `-${window.scrollY}px`;
     } else {
-      const scrollY = document.body.style.top;
       document.body.style.position = "";
-      document.body.style.top = "";
-      window.scrollTo(0, parseInt(scrollY || "0") * -1);
     }
   }, [isModalOpen]);
 
